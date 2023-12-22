c5af22ec49166dacff13c57a1a7c9cdbd844db65
Paweł Gabryś <p.gabrys@onet.eu>
2023-12-22 10:54:13 +0100

    build 0.9

--- certup.py
+++ certup.py
@@ -460,7 +460,7 @@ def ls_certs_pyjks():
     def print_aliases(keystore):
         result = ""
         for alias in keystore.certs.split(":")[0:2]:
-            result += f"{alias[0] + ":" + alias[1] if len(alias) > 1 else alias[0]} | "
+            result += f"{alias[0] + ':' + alias[1] if len(alias) > 1 else alias[0]} | "
         print(result[:-3])  # Usuń ostatnią pustą pętlę
 
     def print_certdate(keystore):
