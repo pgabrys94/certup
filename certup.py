137f0cd88472155f20c3805b4211b0e61e591a8a
Paweł Gabryś <p.gabrys@onet.eu>
2023-12-22 11:19:43 +0100

    build 0.9

--- certup.py
+++ certup.py
@@ -465,7 +465,7 @@ def ls_certs_pyjks():
             result += alias
             formatted = "{:<{}}".format(alias, width)
             print(formatted, end="\n" if i % column == 0 else " ")
-            print()
+        print()
 
     def print_certdate(keystore):
         alias = input("Podaj alias certyfikatu ([enter] - powrót): ")
