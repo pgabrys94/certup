ca53738a0b84c9696495af364293a65a5cbcec1f
Paweł Gabryś <p.gabrys@onet.eu>
2023-12-28 07:45:16 +0100

    v1.16 -uzupełnienie dokumentacji

--- certup.py
+++ certup.py
@@ -14,7 +14,7 @@ from cryptography.hazmat.primitives import serialization
 
 # Informacje
 name = "CertUp"
-version = 1.15
+version = 1.16
 author = "PG/DASiUS"    # https://github.com/pgabrys94
 
 # Zmienne globalne:
